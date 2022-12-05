2862a371769300c9f0230830c563244a9f76fbe1
Quinn <115659596+QuinnanaSmith@users.noreply.github.com>
2022-12-05 14:41:39 -0500

    push4

---
+++ calculator/js/calc.js
@@ -0,0 +1,34 @@
+const result = document.getElementById
+("answer");
+
+function resultAssign() {
+    num = 3 + 5 + 58 + 500 + 2;
+    result.innerHTML = num;
+    console.log ("Variable num represents the number: " + num);
+}
+
+
+function multiply(n1, n2) {
+    let multiplication = n1 * n2;
+result.innerHTNL = multiplication;
+}
+
+function divide(b1, b2) {
+    let divide = b1 / b2;
+result.innerHTNL = divide;
+}
+
+function subtract(t1, t2) {
+    let subtract = t1 - t2;
+result.innerHTNL = subtract;
+}
+function add(y1, y2) {
+    let add = y1 - y2;
+result.innerHTNL = add;
+
+
+
+}
+
+
+
