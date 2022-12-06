3664b738227f1547962a2b8afba009cb1b26eae3
Quinn <115659596+QuinnanaSmith@users.noreply.github.com>
2022-12-06 14:29:57 -0500

    dec6th

--- lightbox/js/light.js
+++ lightbox/js/light.js
@@ -13,7 +13,7 @@ function openLightbox(){
     //display images src inside of lightbox
     lightbox.src = this.src;
 }
-
+//image
 let coffeeImage = document.getElementById
 ('coffeeImage');
 
@@ -24,7 +24,92 @@ function closeLightbox(){
     ('display');
     lightbox.src = "";
 }
+lightboxBackground.onclick = closeLightbox;
+lightboxCloser.onclick = closeLightbox;
 
+
+//image1
+let coffeeImage1 = document.getElementById
+('coffeeImage1');
+
+coffeeImage1.onclick = openLightbox;
+
+function closeLightbox(){
+    lightboxContainer.classList.remove
+    ('display');
+    lightbox.src = "";
+}
+lightboxBackground.onclick = closeLightbox;
+lightboxCloser.onclick = closeLightbox;
+
+//image2
+let coffeeImage2 = document.getElementById
+('coffeeImage2');
+
+coffeeImage2.onclick = openLightbox;
+
+function closeLightbox(){
+    lightboxContainer.classList.remove
+    ('display');
+    lightbox.src = "";
+}
+lightboxBackground.onclick = closeLightbox;
+lightboxCloser.onclick = closeLightbox;
+
+//image3
+let coffeeImage3 = document.getElementById
+('coffeeImage3');
+
+coffeeImage3.onclick = openLightbox;
+
+function closeLightbox(){
+    lightboxContainer.classList.remove
+    ('display');
+    lightbox.src = "";
+}
+lightboxBackground.onclick = closeLightbox;
+lightboxCloser.onclick = closeLightbox;
+
+//image4
+let coffeeImage4 = document.getElementById
+('coffeeImage4');
+
+coffeeImage4.onclick = openLightbox;
+
+function closeLightbox(){
+    lightboxContainer.classList.remove
+    ('display');
+    lightbox.src = "";
+}
+lightboxBackground.onclick = closeLightbox;
+lightboxCloser.onclick = closeLightbox;
+
+//image5
+let coffeeImage5 = document.getElementById
+('coffeeImage5');
+
+coffeeImage5.onclick = openLightbox;
+
+function closeLightbox(){
+    lightboxContainer.classList.remove
+    ('display');
+    lightbox.src = "";
+}
+lightboxBackground.onclick = closeLightbox;
+lightboxCloser.onclick = closeLightbox;
+
+//image6
+let coffeeImage6 = document.getElementById
+('coffeeImage6');
+
+coffeeImage6.onclick = openLightbox;
+
+function closeLightbox(){
+    lightboxContainer.classList.remove
+    ('display');
+    lightbox.src = "";
+}
+lightboxBackground.onclick = closeLightbox;
 lightboxCloser.onclick = closeLightbox;
 
 
