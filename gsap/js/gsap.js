f22590e79bfe90e29b3906ec423c842be7c7f596
Quinn <115659596+QuinnanaSmith@users.noreply.github.com>
2022-12-12 13:11:40 -0500

    dec12

--- gsap/js/gsap.js
+++ gsap/js/gsap.js
@@ -1,4 +1,4 @@
-gsap.from("chucked", {
+gsap.from(".chucked", {
 duration:5,
 rotation: 720,
 ease: "bounce",
@@ -6,3 +6,29 @@ stagger: 0.5,
 x:-500,
 y: -500,
 });
+
+gsap.from(".headline", {
+    duration: 2.5,
+    stagger: 0.3,
+    opacity:0,
+    y:380
+})
+
+gsap.from(".paraAnimate", {
+    duration: 2,
+    stagger: 0.5,
+    opacity:0,
+    y: 530
+    
+
+
+})
+
+gsap.from(".images", {
+duration: 2,
+stagger: 0.9,
+ease: "slide",
+opacity:0,
+y: -430
+})
+
